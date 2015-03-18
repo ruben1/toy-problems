9924a685831004f2ab712b5280f91795e1a455e5
Ruben Vicario Gonzalez <ruben1586@gmail.com>
2015-03-18 14:06:40 -0700

    rename seatCircle

---
+++ anagramDetection.js
@@ -0,0 +1,12 @@
+/*
+Write a function that accepts two parameters, a parent and a child string. 
+Determine how many times the child string - or an anagram of the of the child string - appears 
+in the parent string. There is a solution which can be done in near instant time.
+
+Ex: f('AdnBndAndBdaBn', 'dAn') // 4 ("Adn", "ndA", "dAn", "And")
+Ex: f('AbrAcadAbRa', 'cAda') // 2
+*/
+
+var anagramDetection = function() {
+
+};
