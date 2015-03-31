dfd95e1c90601ca1e593c303e2e16f6b5025289a
Ruben Vicario Gonzalez <ruben1586@gmail.com>
2015-03-30 17:36:25 -0700

    implement quickSort in-place/constant space

--- quickSort.js
+++ quickSort.js
@@ -4,6 +4,8 @@ Write quick sort
 
 */
 
+/*
+Implementation without taking into account space complexity
 
 var quickSort = function(array) {
   if(array.length === 0 || array.length === 1) {
@@ -24,4 +26,51 @@ var quickSort = function(array) {
   }
 
   return quickSort(smallerElems).concat(pivot).concat( quickSort(biggerElems) );
+};
+*/
+
+//Implementation with pure recursion and in-place/constant space complexity
+var quickSort = function(array, from, to) {
+  from = from === undefined ? 0 : from;
+  to = to === undefined ? array.length - 1 : to;
+
+  if(from >= to) {
+    return;
+  }
+
+  var pivot = array[from];
+
+  var min = from;
+  var max = to;
+
+  var free = min;
+  
+  while(min < max) {
+    if(free === min) {
+      if(array[max] <= pivot) {
+        array[free] = array[max];
+        min += 1;
+        free = max;
+      } else {
+        max -= 1;
+      }
+    } else if(free === max) {
+      if(array[min] >= pivot) {
+        array[free] = array[min];
+        max -= 1;
+        free = min;
+      } else {
+        min += 1;
+      }
+    } else {
+      throw 'Chiquillo, que ejz ezto'
+    }
+  }
+
+  array[free] = pivot;
+
+  quickSort(array, from, free - 1)
+  quickSort(array, free + 1, to);
+
+  return array; 
 };
